f487d353ebf8fa66019a7d19e31773360056de5b
jeff <abdallahjeff92@gmail.com>
2025-07-31 15:26:24 +0100

    the props added

--- react apps/components/content.jsx
+++ react apps/components/content.jsx
@@ -2,27 +2,27 @@ import '../src/App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
-export default function Content()
+export default function Content({img , title , country , googleMapsLink , dates , text })
 {
     return (
         <> 
-        <div className='entrydata'>
-                <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="japan" />
-                <div className='description'>
-                    <div className='location'>
-                    <FontAwesomeIcon icon={faLocationDot} className="location-icon" />
-                    <p>JAPAN</p>
-                    <a href="">View on Google Maps</a>
+            <div className='entrydata'>
+                    <img src={img.src} alt={img.alt} />
+                    <div className='description'>
+                        <div className='location'>
+                        <FontAwesomeIcon icon={faLocationDot} className="location-icon" />
+                        <p>{country}</p>
+                        <a href={googleMapsLink}>View on Google Maps</a>
+                        </div>
+                        
+                        <h1>{title}</h1>
+                        <br />
+                        <h2>{dates}</h2>
+                        <br />
+                        <p>{text}</p>
                     </div>
-                    
-                    <h1>Mount Fuji</h1>
-                    <br />
-                    <h2>12 Jan, 2023 - 24 Jan, 2023</h2>
-                    <br />
-                    <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists</p>
-                </div>
 
-        </div>
+            </div>
         </>
     )
 }
