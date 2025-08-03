aeb0d4946fc918bd6c360c7c4d250f6439ffb122
jeff <abdallahjeff92@gmail.com>
2025-08-03 19:23:57 +0100

    the list state

--- react apps/components/Main.jsx
+++ react apps/components/Main.jsx
@@ -3,17 +3,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import styles from '../src/Main.module.css';
 
+
+let ingredientlist = ["milk" , "tomato ", "potato"];
+console.log(ingredientlist);
+
+
+let ingrlist = ingredientlist.map((ing )=> {
+    return <li  key={ing}> {ing} </li>
+})
+
+function Check(e)
+{
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const NewIngridient = formData.get("Ingredients")
+    ingredientlist.push(NewIngridient);
+}
+
+console.log(ingrlist);
+
 export default function Main() {
     return (
         <main>
-            <form className={styles.addingredientform}>
+            <form onSubmit={Check} className={styles.addingredientform}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name='Ingredients'
                 />
                 <button>Add ingredient</button>
             </form>
+                <ul className={styles.ull}>
+                    {ingrlist}
+                </ul>
         </main>
     )
 }
